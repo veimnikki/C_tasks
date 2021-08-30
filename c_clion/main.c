eff2d6ec8ddf1438f026a018af2e1482e02c8228
nikka <veimnikki@gmail.com>
2021-08-30 22:00:41 +0300

    clion

--- c_clion/main.c
+++ c_clion/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int N = 6;
+    int mas[] = {1, 2, 3, 4, 5, 6};
+    for (int i = 0; i < N; i++) {
+        if (i == N-1) {
+            printf("%d", mas[i]);
+        } else {
+            printf("%d, ", mas[i]);
+        }
+    }
     return 0;
 }
