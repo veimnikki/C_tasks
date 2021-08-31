05ea7e59669dd937a8671b87c96220e36677c2d6
nikka <veimnikki@gmail.com>
2021-08-31 11:29:56 +0300

    clion

--- c_clion/main.c
+++ c_clion/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int N = 6;
-    int mas[] = {1, 2, 3, 4, 5, 6};
-    for (int i = 0; i < N; i++) {
-        if (i == N-1) {
-            printf("%d", mas[i]);
-        } else {
-            printf("%d, ", mas[i]);
-        }
-    }
+    int a, b, c, d;
+    srand(time(NULL));
+    d = rand() % 900 + 100;
+    printf("%d\n", d);
+    a = d / 100;
+    b = (d % 100 -  d % 10) / 10;
+    c = d % 10;
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    printf("%d\n", a + b + c);
+    printf("%d", a * b * c);
     return 0;
 }
